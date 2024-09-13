493f9cfeb6a5271a31c1ef93cd523c2d884a1e17
Gabriel Soares <161614457+Bielxs633@users.noreply.github.com>
2024-09-13 17:00:54 -0300

    Add files via upload

---
+++ Aula - Missão 03/Exercício 02/Modulo/Medias.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ *  Objetivo: criar um sistema que gerencie as médias escolares de uma universidade
+ *  Data: 13/09/24
+ *  Autor: Gabriel
+ *  Versão: 1.0
+ * 
+ **/
+
+function gerenciarNotas(nomeAluno, nomeProfessor, sexoAluno, sexoProfessor, nomeCurso, nomeDisciplina, nota1, nota2, nota3, nota4, notaExame){
+
+
+    // Validação de dados em branco
+    if ( nomeAluno == '' || nomeProfessor == '' || sexoAluno == '' || sexoProfessor == '' || nomeCurso == '' || nomeCurso == '' || nomeDisciplina == '' || nota1, nota2, nota3, nota4 == '' || notaExame == ''){
+        return '==================================================  \n|ERRO. É Obrigatório a Entrada de Todos os Valores.  \n==================================================)'
+    }
+}
