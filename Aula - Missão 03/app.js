e4075a4cb4907aae80dc3d8dbde98017bd2e780e
Gabriel Soares <161614457+Bielxs633@users.noreply.github.com>
2024-09-20 16:57:36 -0300

    Add files via upload

---
+++ Aula - Missão 03/app.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ *  Objetivo: Juntar todas as Funções
+ *  Data: 20/09/24
+ *  Autor: Gabriel
+ *  Versão: 1.0
+ * 
+ **/
+
+var exe1 = require('./Exercício 01/Modulo/IMC')
+var exe2 = require('./Exercício 02/Modulo/Medias')
+var exe3 = require('./Exercício 03/Modulo/Tabuada')
+var exe4 = require('./Exercício 04/Modulo/Fatorial')
+var exe5 = require('./Exercício 05/Modulo/par e impar')
+
+function menu(){
+    
+}
