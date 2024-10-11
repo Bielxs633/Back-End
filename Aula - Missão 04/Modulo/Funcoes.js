91c45852043a3f2f334eb1e771e845d7b491310f
Gabriel Soares <161614457+Bielxs633@users.noreply.github.com>
2024-10-11 17:15:02 -0300

    Add files via upload

---
+++ Aula - Missão 04/Modulo/Funcoes.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ *  Objetivo: Criar todas as funções em um arquivo a parte para que no futuro possamos integrar em uma “API bem legal”, que terá como objetivo trazer informações sobre os estados do Brasil.
+ *  Data: 11/10/24
+ *  Autor: Gabriel
+ *  Versão: 1.0
+ * 
+ **/
+
